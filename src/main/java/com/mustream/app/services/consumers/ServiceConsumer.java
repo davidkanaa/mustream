969bd1e27d8543f5ac41d5304a1a2241c49af0fc
davidkanaa <david.kanaa@centrale-marseille.fr>
2016-04-11 20:09:04 -0400

    Added ServiceRegister, ServiceConsumer, Spotify and SoundCloud. Implemented the search service.

---
+++ src/main/java/com/mustream/app/services/consumers/ServiceConsumer.java
@@ -0,0 +1,33 @@
+package com.mustream.app.services.consumers;
+
+import com.mustream.api.models.Track;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by davidkanaa on 16-04-11.
+ */
+public abstract class ServiceConsumer {
+
+    protected String id;
+    protected String name;
+    protected String baseURL;
+    protected String clientID;
+
+    public abstract List<Track> search(String terms);
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        ServiceConsumer consumer = (ServiceConsumer) obj;
+        return Objects.equals(id, consumer.id) &&
+                Objects.equals(name, consumer.name) &&
+                Objects.equals(baseURL, consumer.baseURL);
+    }
+}
--- src/main/java/com/mustream/app/services/impl/SearchApiServiceImpl.java
+++ src/main/java/com/mustream/app/services/impl/SearchApiServiceImpl.java
@@ -1,11 +1,18 @@
 package com.mustream.app.services.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mustream.api.models.Track;
+import com.mustream.app.services.consumers.ServiceConsumer;
+import com.mustream.app.services.register.ServiceRegister;
 import com.mustream.processes.exceptions.NotFoundException;
 import com.mustream.api.services.SearchApiService;
 import com.mustream.processes.utils.ApiResponseMessage;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import java.util.ArrayList;
+import java.util.List;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-04-10T04:15:39.662Z")
 public class SearchApiServiceImpl extends SearchApiService {
@@ -13,6 +20,22 @@ public class SearchApiServiceImpl extends SearchApiService {
     public Response search(String query, SecurityContext securityContext)
             throws NotFoundException {
         // do some magic!
+        List<Track> results = new ArrayList<>();
+        for (ServiceConsumer consumer:
+                ServiceRegister.getInstance_().getServiceConsumers()) {
+            results.addAll(consumer.search(query));
+        }
+
+        // Just for pretty printing ... will be deleted
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return Response.ok().entity(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(results)).build();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        // Here is the true version
+        // return Response.ok().entity(results).build();
+
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
 }
